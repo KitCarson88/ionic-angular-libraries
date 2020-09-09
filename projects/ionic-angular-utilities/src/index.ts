723e5b45b5544c182caadd3922ef1529a9b19b2d
Simone Colazzo <colsimo88@gmail.com>
2020-09-09 10:00:36 +0200

    added debounce button component

--- projects/ionic-angular-utilities/src/index.ts
+++ projects/ionic-angular-utilities/src/index.ts
@@ -1,4 +1,8 @@
+///////////// Components
 export * from './lib/components/centralizer/centralizer.component';
 export * from './lib/components/centralizer/centralizer.module';
+
+///////////// Directives
+export * from './lib/directives/debounce-click/debounce-click.directive';
 
 export * from './lib/ionic-angular-utilities.module';
